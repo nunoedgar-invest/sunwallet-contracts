ddee9b54ad45d45bd9858c98dabff60b7412522b
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-08-25 21:12:15 +0300

    Added fixed initial supply to constructor

--- contracts/SunCoin.sol
+++ contracts/SunCoin.sol
@@ -8,12 +8,13 @@ import "./ERC20Permit.sol";
  * @dev Implementation of the Sun Coin (SUN) ERC20 smart contract.
  */
 contract SunCoin is ERC20Permit, ERC20Burnable {
+  uint256 private constant INITIAL_BALANCE = 100000000 ether;
+
   constructor (
     string memory name,
     string memory symbol,
-    address initialAccount,
-    uint256 initialBalance
+    address initialAccount
   ) public ERC20Permit(name, symbol) {
-    _mint(initialAccount, initialBalance);
+    _mint(initialAccount, INITIAL_BALANCE);
   }
 }
