b1733819203bfa12b8efb2104a1ecf12b8081434
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-09-03 08:14:31 +0300

    Updated the auto-deploy script

--- migrations/2_deploy_factory.js
+++ migrations/2_deploy_factory.js
@@ -6,12 +6,12 @@ const UniswapV2Router02 = artifacts.require('UniswapV2Router02')
 
 const { UNISWAP_FACTORY, WETH_TOKEN } = process.env
 
+
 module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
     console.log('Deployer:', accounts[0])
-    const SunTokenConfigs = ['SUN', 'Sun Coin', accounts[0]]
 
-    const SUNTokenInstance = await deployer.deploy(SunCoin, ...SunTokenConfigs)
+    const SUNTokenInstance = await deployer.deploy(SunCoin, accounts[0])
     console.log('SUN token:', SUNTokenInstance.address)
 
     const UniswapRouterInstance = await deployer.deploy(UniswapV2Router02, UNISWAP_FACTORY, WETH_TOKEN, SUNTokenInstance.address)
